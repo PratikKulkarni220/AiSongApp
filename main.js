008034091d9e8c6ab7e8128a3bb7af506b94145e
PratikKulkarni220 <73877259+PratikKulkarni220@users.noreply.github.com>
2021-03-07 14:48:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preload(){
+    music =loadSound("music.mp3");
+    music =loadSound('music_2.mp3');
+}
+
+
+function setup(){
+    canvas = createCanvas(780, 600);
+
+    canvas.position(440, 400);
+
+     video = createCapture(VIDEO);
+
+     video.hide();
+}
